7e0048809ac05960dc506f48e32e614dc926dd9f
rcianghe <305604094+rcianghe@users.noreply.github.com>
2026-08-13 12:38:44 -0400

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,25 +1,23 @@
 importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging-compat.js');
 
-// Configuración de tu proyecto Firebase
 firebase.initializeApp({
-  apiKey: "AIzaSyA237FUvmXmEXDuhs7Mz2c0...", // Tómala de Firebase Project Settings
+  apiKey: "AIzaSyA237FUvmXmEXDuhs7Mz2cO1TV6kmEsqo0",
   authDomain: "chat-app-accc3.firebaseapp.com",
   projectId: "chat-app-accc3",
   storageBucket: "chat-app-accc3.firebasestorage.app",
   messagingSenderId: "240530471180",
-  appId: "1:240530471180:web:58db290f9c..."
+  appId: "1:240530471180:web:58db290f9ce9b430f6b129"
 });
 
 const messaging = firebase.messaging();
 
-// Manejar notificaciones en segundo plano (pantalla bloqueada / app cerrada)
 messaging.onBackgroundMessage(function(payload) {
-  const notificationTitle = payload.notification?.title || 'Club Tenis Estadio Quilicura';
+  console.log('Mensaje recibido en segundo plano:', payload);
+  const notificationTitle = payload.notification?.title || 'Club de Tenis Quilicura';
   const notificationOptions = {
-    body: payload.notification?.body || '¡Hay novedades en el ranking/escalerilla!',
+    body: payload.notification?.body || '',
     icon: '/icon-192.png'
   };
-
   self.registration.showNotification(notificationTitle, notificationOptions);
 });
